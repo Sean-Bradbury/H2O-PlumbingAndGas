400e01334a2f6c6b560f408e26c1dae90c7adc84
sean-bradbury <sean.m.bradbury@googlemail.com>
2021-01-31 11:12:58 +0000

    form testing

--- includes/content-right-sidebar.php
+++ includes/content-right-sidebar.php
@@ -2,14 +2,10 @@
   <h3 class="grey-heading-noline mb-4">Ready To Get In Touch?</h3>
   <p>Please fill out the form below</p>
   <form method="post" name="contact-form" action="contact-form-handler.php">
-    <input type="text" name="name" placeholder="Name" class="d-block mb-2 p-2 w-100">
-    <input type="email" name="email" placeholder="Email" class="d-block mb-2 p-2 w-100">
-    <textarea name="message" name="message" placeholder="Please enter your message" cols="30" rows="10" class="d-block mb-2 p-2 w-100"></textarea>
+    <input type="text" name="name" placeholder="Name" class="d-block mb-2 p-2 w-100" required>
+    <input type="email" name="email" placeholder="Email" class="d-block mb-2 p-2 w-100" required>
+    <textarea name="message" name="message" placeholder="Please enter your message" cols="30" rows="10" class="d-block mb-2 p-2 w-100" required></textarea>
     <button type="submit" value="Submit" class="btn btn-red">Send Enquiry</button>
     
   </form>
 </div>
-
-<script language=”JavaScript”>
-
-var frmvalidator = new Validator(“contactform”); frmvalidator.addValidation(“name”,”req”,”Please provide your name”); frmvalidator.addValidation(“email”,”req”,”Please provide your email”); frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”); </script>
